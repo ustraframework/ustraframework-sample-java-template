4e979fb3c75c91cfa0e33c7fe148bf3bb7f5e092
Roy Lee <keymasroy@gmail.com>
2022-03-18 10:46:12 +0900

    기본 템플릿 최초 커밋

---
+++ java-sample-template/src/main/java/com/gsitm/ustra/java/sample/template/SampleTemplateController.java
@@ -0,0 +1,36 @@
+package com.gsitm.ustra.java.sample.template;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+
+/**
+ * 샘플 템플릿 컨트롤러
+ * @author RoyLee
+ *
+ */
+@Api("샘플 템플릿 API")
+@RestController
+@RequestMapping("/api/sample/template")
+public class SampleTemplateController {
+
+	@Autowired private SampleTemplateService sampleTemplateService;
+
+	@GetMapping
+	@ApiOperation(value = "전체 코드 목록 조회", notes = "<strong>코드 전체 목록</string>을 반환")
+	public List<SampleTemplateModel> getAll(@ApiParam("사용 여부") String useYn) {
+
+		SampleTemplateModel.Criteria criteria = new SampleTemplateModel.Criteria();
+		criteria.setUseYn(useYn);
+
+		return this.sampleTemplateService.getAll(criteria);
+	}
+
+}
