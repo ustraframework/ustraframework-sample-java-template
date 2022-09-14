f2cbb3047f0ac63873274f51514b35204285d04b
jhjcoding <77096242+jhjcoding@users.noreply.github.com>
2022-09-14 21:56:41 +0900

    Delete ustraframework-sample-microservice-interface directory

--- ustraframework-sample-microservice-interface/main/src/main/java/com/gsitm/ustra/java/sample/microserviceinterface/main/client/MicroServiceInterfaceClient.java
+++
@@ -1,7 +0,0 @@
-package com.gsitm.ustra.java.sample.microserviceinterface.main.client;
-
-import java.lang.reflect.Method;
-
-public interface MicroServiceInterfaceClient {
-	Object request(Method method, Object[] args);
-}
--- ustraframework-sample-microservice-interface/main/src/main/java/com/gsitm/ustra/java/sample/microserviceinterface/main/interfaces/SampleMSI.java
+++
@@ -1,8 +0,0 @@
-package com.gsitm.ustra.java.sample.microserviceinterface.main.interfaces;
-
-import com.gsitm.ustra.java.sample.microserviceinterface.main.annotations.MicroServiceInterfaceAnnotation;
-
-@MicroServiceInterfaceAnnotation("api1")
-public interface SampleMSI {
-
-}
